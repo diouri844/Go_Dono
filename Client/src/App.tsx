8ed18e2a08aa8a035981b8d401d3e3a0f3362df7
Salah Iddine <SalahIddine.Diouri@usmba.ac.ma>
2022-11-08 20:42:51 +0100

    update main app

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 // use the box component from maintain core :
-import { Box, List } from '@mantine/core';
+import { Box, Title, List, ThemeIcon  } from '@mantine/core';
+import { IconCircleCheck, IconCircleDashed } from '@tabler/icons';
 // import add todo modal component :
 
 import AddTodo from './component/AddTodo';
@@ -13,9 +15,9 @@ export const baseUrl = "http://localhost:3030";
 
 export interface Todo{
   id:number,
-  Title: string,
-  Body:string,
-  Done:boolean
+  title: string,
+  body:string,
+  done:boolean
 };
 
 function App() {
@@ -23,22 +25,39 @@ function App() {
   // create an fetch function :
   const fetcher = (url: string)=>fetch(baseUrl+"/"+url).then(resp => { resp.json()});
   // step 2: fetch data from the get go-endpoint :
+  const [listtodo, setlisttodo] = useState([]);
   const { data, mutate } = useSWR<Todo[]>('api/Todos', fetcher);
-  // check if error not null :
-
+  useEffect(() => {
+    // Update the document title using the browser API
+    console.log (" data loaded ");
+    setlisttodo(data);
+    console.log(listtodo);
+  },[mutate,data]);
   return (
     <Box>
-      <List spacing="xs" size="sm" mb={12} center listStyleType="disc">
-      { data?.map( (todo) => {
-        console.log( todo);
-        return( 
-        <List.Item key={`todo__${todo.id}`}  color="blue"> { todo.Title }</List.Item>
-        );
-      })} 
-      </List>
-    <AddTodo mutate={ mutate }/>
-    </Box>
-  )
-}
+        <Title order={2} mb={12} color="cyan"> Todo List  </Title>
+        <List 
+        center
+        mb={15} 
+        size="sm"
+        icon={
+                <ThemeIcon color="blue" size={24} radius="xl">
+                <IconCircleDashed  size={16} />
+                </ThemeIcon>
+        }
+        >
+        {
+          listtodo?.map( (current) => { 
+            console.log(current)
+            return <List.Item 
+            className='todo_item'
+            key={`todo_list_${current.id}`}
+            mb={12}
+            > { current.body }</List.Item>
+            })}
+        </List>
+        <AddTodo mutate={ mutate }/>
+      </Box>
+  )}
 
 export default App
