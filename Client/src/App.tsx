98f7e2e89070e8f0e7af3a138562a4dc344244c0
Salah Iddine <SalahIddine.Diouri@usmba.ac.ma>
2022-11-08 15:16:48 +0100

    Update Client

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,33 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import './App.css';
+// use the box component from maintain core :
+import { Box, List } from '@mantine/core';
+// import add todo modal component :
+
+import AddTodo from './component/AddTodo';
+// import swr-data-fetcher :
+import useSWR from 'swr';
+// define url base :
+export const baseUrl = "http://localhost:3030";
+
+// create an interface for an todo object :
+
+export interface Todo{
+  id:number,
+  Title: string,
+  Body:string,
+  Done:boolean
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  //declare my state :
+  // create an fetch function :
+  const fetcher = (url: string)=>fetch(baseUrl+"/"+url).then(resp => { resp.json()});
+  // step 2: fetch data from the get go-endpoint :
+  const { data, mutate } = useSWR<Todo[]>('api/Todos', fetcher);
+  // check if error not null :
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <Box>
+      <List spacing="xs" size="sm" mb={12} center listStyleType="disc">
+      { data?.map( (todo) => {
+        console.log( todo);
+        return( 
+        <List.Item key={`todo__${todo.id}`}  color="blue"> { todo.Title }</List.Item>
+        );
+      })} 
+      </List>
+    <AddTodo mutate={ mutate }/>
+    </Box>
   )
 }
 
